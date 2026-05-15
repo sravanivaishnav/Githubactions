3987d790d074cf213bd6521ce2abe94019c12be9
sravanivaishnav <151617982+sravanivaishnav@users.noreply.github.com>
2026-05-16 00:40:34 +0530

    c8

--- assesmnet project/infra/varibles.tf
+++ assesmnet project/infra/varibles.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-  type = string
+ Default {}
 }
 
 variable "location" {
-  type = string
+ Default {}
 }
 
 variable "storage_account_name" {
-  type = string
+ Default {}
 }
