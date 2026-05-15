69b4e1c7075498075389220966d378de038681de
sravani vangara <sravani.vangara@accenture.com>
2026-05-15 19:24:29 +0530

    chore(terraform): fix HCL, set azurerm v4.73.0 and clean tfvars

--- assesmnet project/infra/main.tf
+++ assesmnet project/infra/main.tf
@@ -1,29 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.73.0"
+    }
+  }
+}
+
 provider "azurerm" {
-	features {}
+  features {}
 }
 
 variable "resource_group_name" {
-	type = string
+  type = string
 }
 
 variable "location" {
-	type = string
+  type = string
 }
 
 variable "storage_account_name" {
-	type = string
+  type = string
 }
 
 resource "azurerm_resource_group" "rg" {
-	name     = var.resource_group_name
-	location = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "sa" {
-	name                     = var.storage_account_name
-	resource_group_name      = azurerm_resource_group.rg.name
-	location                 = azurerm_resource_group.rg.location
-	account_tier             = "Standard"
-	account_replication_type = "LRS"
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
