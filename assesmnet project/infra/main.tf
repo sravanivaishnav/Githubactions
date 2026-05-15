a2baea68defd151862e3ee2744cb22eec853dfc0
sravanivaishnav <151617982+sravanivaishnav@users.noreply.github.com>
2026-05-15 18:50:26 +0530

    commit2
    Refactor Terraform configuration to simplify resource definitions and improve variable usage.

--- assesmnet project/infra/main.tf
+++ assesmnet project/infra/main.tf
@@ -1,99 +1,17 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-  required_version = ">= 1.0"
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "random_string" "suffix" {
-  length  = 5
-  upper   = false
-  special = false
-  numeric = true
-}
-
-locals {
-  rg_name        = "rg-${var.project_name}-${var.environment}"
-  plan_name      = "asp-${var.project_name}-${var.environment}-${random_string.suffix.result}"
-  webapp_name    = "app-${var.project_name}-${var.environment}-${random_string.suffix.result}"
-  storage_name   = substr(lower("st${var.project_name}${var.environment}${random_string.suffix.result}"), 0, 24)
-  container_name = "appdata"
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = local.rg_name
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_service_plan" "plan" {
-  name                = local.plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = local.storage_name
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-  }
 }
-
-resource "azurerm_storage_container" "container" {
-  name                  = local.container_name
-  storage_account_id    = azurerm_storage_account.storage.id
-  container_access_type = "private"
-}
-
-resource "azurerm_linux_web_app" "webapp" {
-  name                = local.webapp_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
-  https_only          = true
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  site_config {
-    always_on = true
-
-    application_stack {
-      node_version = "20-lts"
-    }
-  }
-
-  app_settings = {
-    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
-    STORAGE_ACCOUNT_NAME           = azurerm_storage_account.storage.name
-    STORAGE_CONTAINER_NAME         = azurerm_storage_container.container.name
-    PORT                           = "8080"
-  }
-
-  tags = {
-    environment = var.environment
-    project     = var.project_name
-  }
-}
-
-resource "azurerm_role_assignment" "blob_data_contributor" {
-  scope                = azurerm_storage_account.storage.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
-}
+``
