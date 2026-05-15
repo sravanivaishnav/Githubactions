6bef6b38a0203f7fdaa0bfd5b33970a7eba84b14
sravani vangara <sravani.vangara@accenture.com>
2026-05-15 16:38:23 +0530

    Add minimal azurerm provider and example resource group

--- assesmnet project/infra/main.tf
+++ assesmnet project/infra/main.tf
@@ -1,2 +1,20 @@
-#its sample
+terraform {
+	required_providers {
+		azurerm = {
+			source  = "hashicorp/azurerm"
+			version = "~> 3.0"
+		}
+	}
+	required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+	features {}
+}
+
+resource "azurerm_resource_group" "example" {
+	name     = "rg-example-terraform"
+	location = "East US"
+}
+
 
